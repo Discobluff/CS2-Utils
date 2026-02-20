d4ae71787d9cd88c3babe0259e55c8bdca4ecaa3
Hugo Dartoy <hugo.dartoy@telecomnancy.eu>
2026-02-20 15:59:05 +0100

    feat(backend): route for assets

---
+++ src/backend/src/main/java/com/cs2utils/api/Assets.java
@@ -0,0 +1,20 @@
+package com.cs2utils.api;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+// import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Assets {
+
+    @GetMapping("/assets")
+    public byte[] getAsset(@RequestParam(value = "type", defaultValue = "") String type, @RequestParam(value = "name", defaultValue = "") String name) throws IOException {
+        return Files.readAllBytes(Paths.get("src/main/assets/" + type + "/" + name + ".png"));
+    }
+}
